0156a8d12f38b7c0cd28f4362261341d88e1b29f
J-CamiloG <jc6381878@gmail.com>
2024-07-07 19:26:09 -0500

    Correcion de compilacion, y estructura del codigo por escape de comillas

--- src/app/components/Aside.tsx
+++ src/app/components/Aside.tsx
@@ -4,9 +4,9 @@ export default function AsideDashboard() {
     return(
         <aside className="bg-gradient-to-br from-gray-800 to-gray-900 -translate-x-80 fixed inset-0 z-50 my-4 ml-4 h-[calc(100vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0">
             <div className="relative border-b border-white/20">
-            <p  className="flex items-center gap-4 py-6 px-8" >
-                <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-white">Menu</h6>
-            </p>
+                <p  className="flex items-center gap-4 py-6 px-8" >
+                    <span className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-white">Menu</span>
+                </p>
             <button className="middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-white hover:bg-white/10 active:bg-white/30 absolute right-0 top-0 grid rounded-br-none rounded-tl-none xl:hidden" type="button">
                 <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" aria-hidden="true" className="h-5 w-5 text-white">
@@ -49,7 +49,6 @@ export default function AsideDashboard() {
                 </a>
                 </li>
             </ul>
-
             </div>
         </aside>
     )
